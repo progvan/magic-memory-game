22c2bbd123e288dc5562d0bd6d1df5499ddaafa1
VanUA <vanatom22@gmail.com>
2024-12-02 16:09:43 +0200

    added disabling card selections and turns

--- src/App.js
+++ src/App.js
@@ -16,12 +16,15 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false) 
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))    
-
+      
+      setChoiceOne(null)
+      setChoiceTwo(null)
       setCards(shuffledCards)
       setTurns(0)
     }
@@ -32,6 +35,7 @@ function App() {
 
   useEffect(() => {
     if(choiceOne && choiceTwo) {
+      setDisabled(true)
       if(choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -49,13 +53,16 @@ function App() {
     }
   }, [choiceOne, choiceTwo])
   
-  console.log(cards);
-  
   const resetTurn = () => {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
   }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className='App'>
@@ -69,9 +76,11 @@ function App() {
             card={card}
             handleChoice={handleChoice} 
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
